9a6c40886193c8b8ba1ee2d714d41bfe05aa3495
Jonny Strong <jonnystrong90@gmail.com>
2026-08-27 23:40:52 +0300

    simplify clientManager

--- src/main/java/com/questionnaire/core/ClientManager.java
+++ src/main/java/com/questionnaire/core/ClientManager.java
@@ -1,34 +1,27 @@
 package com.questionnaire.core;
 
-import java.util.List;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import com.questionnaire.core.model.Client;
 
-import lombok.Getter;
-
 public class ClientManager {
     private Map<Long, Client> clients; // <userId, Client>
-    @Getter
-    private List<Client> clientsList;
-    @Getter
-    private AtomicInteger clientsCount;
 
     public ClientManager() {
         this.clients = new ConcurrentHashMap<>();
-        this.clientsList = new CopyOnWriteArrayList<>();
-        this.clientsCount = new AtomicInteger(0);
     }
 
     public boolean put(Client client) {
-        if (this.clients.putIfAbsent(client.getClientId(), client) == null) {
-            this.clientsCount.incrementAndGet();
-            this.clientsList.add(client);
-            return true;
-        }
-        return false;
+        return (this.clients.putIfAbsent(client.getClientId(), client) == null);
+    }
+
+    public long getClientsCount() {
+        return this.clients.size();
+    }
+
+    public Collection<Client> getClientsList() {
+        return this.clients.values();
     }
 }
