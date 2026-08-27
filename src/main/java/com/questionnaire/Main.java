16d6d3a360b1389ff69964bf367439380527b8ca
Jonny Strong <jonnystrong90@gmail.com>
2026-08-28 00:03:49 +0300

    cleanup

--- src/main/java/com/questionnaire/Main.java
+++ src/main/java/com/questionnaire/Main.java
@@ -4,6 +4,6 @@ import com.questionnaire.core.TelegramCom;
 
 public class Main {
     public static void main(String[] args) {
-        TelegramCom telegramCom = new TelegramCom();
+        new TelegramCom();
     }
 }
