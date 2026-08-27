182b71a7a522b83d011836dd2a6e118b79ace28c
Jonny Strong <jonnystrong90@gmail.com>
2026-08-28 00:30:35 +0300

    more strucks

--- src/main/java/com/questionnaire/core/Survey.java
+++ src/main/java/com/questionnaire/core/Survey.java
@@ -3,6 +3,7 @@ package com.questionnaire.core;
 import java.util.List;
 import java.util.Set;
 
+import com.questionnaire.core.enums.ESurveyStatus;
 import com.questionnaire.core.model.Client;
 import com.questionnaire.core.model.Question;
 
@@ -12,9 +13,19 @@ import lombok.Data;
 public class Survey {
     private List<Question> questions;
     private Set<Client> group;
+    private ESurveyStatus status;
 
     public Survey(List<Question> questions, Set<Client> group) {
         this.questions = questions;
         this.group = group;
+        this.status = ESurveyStatus.PRE;
+    }
+
+    public void startSurvey() {
+        this.status = ESurveyStatus.STARTED;
+    }
+
+    public void endSurvey() {
+        this.status = ESurveyStatus.ENDED;
     }
 }
---
+++ src/main/java/com/questionnaire/core/enums/ESurveyStatus.java
@@ -0,0 +1,7 @@
+package com.questionnaire.core.enums;
+
+public enum ESurveyStatus {
+    PRE,
+    STARTED,
+    ENDED
+}
